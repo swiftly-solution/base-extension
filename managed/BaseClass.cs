5481d625c56263894fce80b482e5f93af312f424
skuzzis <andreieurt@gmail.com>
2025-08-03 19:18:34 +0000

    update(files): Move to XMake

---
+++ managed/BaseClass.cs
@@ -0,0 +1,10 @@
+namespace SwiftlyS2.Extensions.BaseExtension
+{
+    public class BaseExt
+    {
+        public BaseExt()
+        {
+            Console.WriteLine("Base extension class has been initialized");
+        }
+    }
+}
